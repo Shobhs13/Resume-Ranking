17c112de12023c8f4ad02445d611e9c808b1a2ca
Shobhit Sinha <46240995+Shobhs13@users.noreply.github.com>
2020-07-29 00:14:30 +0530

    No Need of BeautifulSoup for this code

--- text_process.py
+++ text_process.py
@@ -3,7 +3,6 @@ import re, string, unicodedata
 import nltk
 import contractions
 import inflect
-from bs4 import BeautifulSoup
 from nltk import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import LancasterStemmer, WordNetLemmatizer
